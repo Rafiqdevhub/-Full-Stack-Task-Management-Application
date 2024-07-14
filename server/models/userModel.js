bfedf583c195d5da87dc62ffbb84344bad989f9d
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-07-14 22:22:25 +0500

    Update user model schema for password minlength validation

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,9 +3,20 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
   },
   {
     timestamps: true,
