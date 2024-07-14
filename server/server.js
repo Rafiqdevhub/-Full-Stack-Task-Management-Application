7c8a4e998e107f87e22fbe84abb92cd3d0d71a18
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-07-14 21:33:32 +0500

    Add CORS support for API endpoints

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
+
 const dbConnection = require("./config/dbConnection");
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 dbConnection();
 
 app.use(express.json());
+
+const origin = process.env.CORS_ORIGIN || "http://localhost:3000";
+
+app.use(
+  cors({
+    origin,
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
