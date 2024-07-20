c398e233f6405b31e111cf4fdc2e49868c006486
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-07-20 10:19:09 +0500

    Add authentication hooks and pages

---
+++ client/src/services/autService.js
@@ -0,0 +1,5 @@
+const autService = () => {
+  return <div></div>;
+};
+
+export default autService;
---
+++ client/src/services/taskService.js
@@ -0,0 +1,5 @@
+const taskService = () => {
+  return <div></div>;
+};
+
+export default taskService;
